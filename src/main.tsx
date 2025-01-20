131bb29fbce4f962fee934fd845315b90020af08
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-20 16:42:44 +0500

    react-router-dom added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Home } from './pages'
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    path: '/',
+    children: [
+      {
+        element: <Home />,
+        path: ''
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
