c226e0576189acfe77f509dd033589be77e5bca8
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-20 16:41:29 +0500

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,60 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		}
-  	}
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        primary: "#2C57A7",
+        on_primary: "#ffffff",
+        primary_container: "#BBCDED",
+        on_primary_container: "#112343",
+
+        secondary: "#495F89",
+        on_secondary: "#ffffff",
+        secondary_container: "#C7D0E1",
+        on_secondary_container: "#1D2637",
+
+        teritary: "#835415",
+        on_teritary: "#ffffff",
+        teritary_container: "#FFDDBA",
+        on_teritary_container: "#2B1700",
+
+        on_surface: "#26292E",
+        on_surface_v: "#5F6674",
+
+        outline: "#A1A7B2",
+        outline_v: "#D0D3D8",
+        background: "#F9F9FA",
+        bg_surface_container: "#EEEDF4",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
