1d6119e22023bae41ee51a787efddc06d2be2de6
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-20 16:44:09 +0500

    new reexports

---
+++ src/components/shared/index.ts
@@ -0,0 +1,8 @@
+export { Logo } from "./logo";
+export { Header } from "./header";
+export { Footer } from "./footer";
+export { LangMenu } from "./lang-menu";
+export { Container } from "./container";
+export { HomeHero } from "./home-hero";
+export { HomeAbout } from "./home-about";
+export { AboutCard } from "./about-card";
