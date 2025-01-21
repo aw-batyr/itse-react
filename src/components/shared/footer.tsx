69b34763b01ac9e064a0388c8f13c25fc23cea2b
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-21 18:57:26 +0500

    ready

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -1,9 +1,26 @@
 import { FC } from "react";
+import { Container, Logo } from "./";
 
-interface Props {
-  className?: string;
-}
+export const Footer: FC = () => {
+  return (
+    <footer className="py-5 bg-bg_surface_container">
+      <Container className="flex flex-col gap-6">
+        <div className="flex items-end justify-between">
+          <Logo />
 
-export const Footer: FC<Props> = ({ className }) => {
-  return <footer className={className}>Footer</footer>;
+          <div className="flex items-center gap-6">
+            <img src="/inst.svg" />
+            <img src="/in.svg" />
+            <img src="/x.svg" />
+          </div>
+        </div>
+
+        <hr className="border-outline_v" />
+
+        <h5 className="text-base text-center text-on_surface_v">
+          ©2025 Все права защищены
+        </h5>
+      </Container>
+    </footer>
+  );
 };
