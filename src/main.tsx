f84de9f0153347fbe2dac4ab13554fc5d15fd64e
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-21 18:57:05 +0500

    b2b page added

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Home } from './pages'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { B2b, Home } from "./pages";
 
 const router = createBrowserRouter([
   {
     element: <App />,
-    path: '/',
+    path: "/",
     children: [
       {
         element: <Home />,
-        path: ''
-      }
-    ]
-  }
-])
+        path: "",
+      },
+      {
+        element: <B2b />,
+        path: "/B2B-B2G",
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
