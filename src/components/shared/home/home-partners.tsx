be8e56c50f113e54f77ca7c32da31a637bec49e9
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-21 18:58:20 +0500

    ready

--- src/components/shared/home/home-partners.tsx
+++ src/components/shared/home/home-partners.tsx
@@ -1,36 +1,84 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { FC } from "react";
-import { Container } from "../";
+import { FC, useCallback, useEffect, useState } from "react";
+import { Container, EmblaDots } from "../";
 import useEmblaCarousel from "embla-carousel-react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 interface Props {
   className?: string;
 }
 
 export const HomePartners: FC<Props> = ({ className }) => {
-  const [embalRef] = useEmblaCarousel();
+  const [emblaRef, emblaApi] = useEmblaCarousel();
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [slides, setSlides] = useState<number[]>([]);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    setSlides(Array.from(Array(emblaApi.slideNodes().length).keys()));
+    const onSelect = () => setSelectedIndex(emblaApi.selectedScrollSnap());
+    emblaApi.on("select", onSelect);
+    onSelect();
+
+    return () => {
+      emblaApi.off("select", onSelect);
+    };
+  }, [emblaApi]);
+
+  const scrollTo = useCallback(
+    (index: number) => {
+      if (emblaApi) emblaApi.scrollTo(index);
+    },
+    [emblaApi]
+  );
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
 
   return (
     <section className={cn("", className)}>
-      <Container className="flex flex-col gap-10">
+      <Container className="flex flex-col gap-10 relative w-full">
         <div className="flex item-center justify-between">
           <h2 className="h2">Участники выставки ITSE 2025</h2>
+
           <Button variant="outline" size={"sm"}>
             Все экспоненты
           </Button>
         </div>
 
-        <div ref={embalRef} className="embla">
+        <button
+          onClick={scrollPrev}
+          className="nav-btn absolute top-[55%] -translate-y-1/2 -left-12"
+        >
+          <ArrowLeft />
+        </button>
+
+        <button
+          onClick={scrollNext}
+          className="nav-btn absolute top-[55%] -translate-y-1/2 -right-12"
+        >
+          <ArrowRight />
+        </button>
+
+        <div ref={emblaRef} className="embla overflow-hidden">
           <div className="embla__container gap-6 flex">
-            <div className="embla__slide size-[184px] basis-[184px] bg-[#D9D9D9]"></div>
-            <div className="embla__slide size-[184px] basis-[184px] bg-[#D9D9D9]"></div>
-            <div className="embla__slide size-[184px] basis-[184px] bg-[#D9D9D9]"></div>
-            <div className="embla__slide size-[184px] basis-[184px] bg-[#D9D9D9]"></div>
-            <div className="embla__slide size-[184px] basis-[184px] bg-[#D9D9D9]"></div>
-            <div className="embla__slide size-[184px] basis-[184px] bg-[#D9D9D9]"></div>
+            {Array.from({ length: 12 }).map((_, i) => (
+              <div
+                key={i}
+                className="embla__slide size-[184px] flex-[0_0_184px] bg-[#D9D9D9]"
+              />
+            ))}
           </div>
         </div>
+
+        <EmblaDots scrollTo={scrollTo} slides={4} active={selectedIndex} />
       </Container>
     </section>
   );
