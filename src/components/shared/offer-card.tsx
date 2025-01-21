a75aaa1df8d655ceb7c056c6c2c0dfd6c71bbe18
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-21 18:58:09 +0500

    ready

--- src/components/shared/offer-card.tsx
+++ src/components/shared/offer-card.tsx
@@ -1,5 +1,7 @@
 import { cn } from "@/lib/utils";
 import { FC } from "react";
+import { Button } from "../ui/button";
+import { ArrowUpRight } from "lucide-react";
 
 interface Props {
   title: string;
@@ -9,11 +11,26 @@ interface Props {
 
 export const OfferCard: FC<Props> = ({ className, title, text }) => {
   return (
-    <article className={cn("px-6 py-8 relative", className)}>
-      <div className="absolute top-0 left-0 bg-gradient-to-r bg-[#2C57A7] from-[#2C57A7] to-[#2C57A7]/20" />
+    <article
+      className={cn(
+        "px-8 py-6 relative overflow-hidden h-[296px] w-full text-on_primary",
+        className
+      )}
+    >
+      <div className="absolute size-full  top-0 left-0 bg-gradient-to-r from-25% from-[#2C57A7] to-[#2C57A7]/20 " />
+      <img
+        src="/offer-1.png"
+        className="absolute -z-10 top-0 right-0 object-cover"
+      />
 
-      <h4 className="text-4xl">{title}</h4>
-      <p>{text}</p>
+      <div className="relative z-20">
+        <h4 className="text-2xl mb-12 z-20">{title}</h4>
+        <p className="text-base w-regular max-w-[360px] z-20">{text}</p>
+
+        <Button className="text-sm px-0 mt-4 py-1.5 z-20" variant={"link"}>
+          Скачать PDF <ArrowUpRight />
+        </Button>
+      </div>
     </article>
   );
 };
