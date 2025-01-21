83fe58d1e49aed8591292e2ec0bbed7884d5cec4
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-21 18:58:31 +0500

    new reexport

--- src/components/shared/home/index.ts
+++ src/components/shared/home/index.ts
@@ -3,4 +3,4 @@ export { HomeTime } from "./home-time";
 export { HomeAbout } from "./home-about";
 export { HomeTheme } from "./home-theme";
 export { HomePartners } from "./home-partners";
-export { HomeOffers } from "./";
+export { HomeOffers } from "./home-offers";
