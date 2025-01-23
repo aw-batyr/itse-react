f4b08703ee8ffc146f2506f5048fd428ec3851cc
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-23 22:58:13 +0500

    font weight changed

--- src/components/shared/about-card.tsx
+++ src/components/shared/about-card.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { FC } from "react";
 
 interface Props {
@@ -8,11 +9,18 @@ interface Props {
 
 export const AboutCard: FC<Props> = ({ className, nums, text }) => {
   return (
-    <article className="px-6 py-4 relative bg-bg_surface_container h-[160px] w-full overflow-hidden">
+    <article
+      className={cn(
+        "px-6 py-4 relative bg-bg_surface_container h-[160px] w-full overflow-hidden",
+        className
+      )}
+    >
       <img src="/bg-logo.svg" className="absolute top-2 right-0" />
-      <h2 className="text-primary text-[32px] leading-[130%] mb-4">{nums}</h2>
+      <h2 className="text-primary text-[32px] semibold leading-[130%] mb-4">
+        {nums}
+      </h2>
 
-      <h4 className="text-on_surface w-regular text-base">{text}</h4>
+      <h4 className="text-on_surface normal text-base">{text}</h4>
     </article>
   );
 };
