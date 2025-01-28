7cefa55d00c0c83d7d81dcde83253d8592160f19
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-28 18:12:08 +0500

    footer mob ready

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -5,7 +5,7 @@ export const Footer: FC = () => {
   return (
     <footer className="py-5 bg-bg_surface_container">
       <Container className="flex flex-col gap-6">
-        <div className="flex items-end justify-between">
+        <div className="flex flex-col md:flex-row gap-6 md:items-end md:justify-between items-center">
           <Logo />
 
           <div className="flex items-center gap-6">
