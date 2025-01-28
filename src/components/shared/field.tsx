61cd727c84738a0fb48eadf19b66337bde66005e
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-28 17:09:39 +0500

    added textarea

--- src/components/shared/field.tsx
+++ src/components/shared/field.tsx
@@ -9,6 +9,7 @@ import {
 import { Input } from "../ui/input";
 import { cn } from "@/lib/utils";
 import { LegacyRef } from "react";
+import { Textarea } from "../ui/textarea";
 
 type Props = {
   control: any;
@@ -19,10 +20,12 @@ type Props = {
   area?: boolean;
   ref?: LegacyRef<HTMLInputElement>;
   type?: string;
+  textArea?: boolean;
   className?: string;
   disabled?: boolean;
   textDark?: boolean;
   supporText?: string;
+  onPrimary?: boolean;
   handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
@@ -36,9 +39,11 @@ export const Field = ({
   className,
   disabled,
   ref,
+  textArea = false,
   textDark,
   supporText,
   handleChange,
+  onPrimary = false,
 }: Props) => {
   return (
     <FormField
@@ -49,6 +54,7 @@ export const Field = ({
           <FormLabel
             className={cn(
               "text-xl",
+              onPrimary && "!text-on_primary",
               textDark ? "text-on_surface" : "text-on_surface_v"
             )}
           >
@@ -57,45 +63,63 @@ export const Field = ({
 
           <FormControl>
             <>
-              {type !== "file" && (
+              {textArea && (
+                <Textarea
+                  rows={3}
+                  {...field}
+                  placeholder={placeholder}
+                  className={cn(
+                    error && "border-[#BA1A1A]",
+                    onPrimary &&
+                      "border-primary_outline_reverse focus:border-white hover:border-white focus:border-1 text-on_primary"
+                  )}
+                />
+              )}
+              {!textArea && type !== "file" ? (
                 <Input
                   type={type}
                   placeholder={placeholder}
                   {...field}
                   disabled={disabled}
-                  className={error && "border-[#BA1A1A]"}
-                />
-              )}
-
-              {/* Обработка файлов */}
-              {type === "file" && (
-                <div className="relative">
-                  <Input
-                    ref={ref}
-                    type="file"
-                    placeholder={placeholder}
-                    onChange={(e) => {
-                      const file = e.target.files?.[0] || null;
-                      console.log("Выбранный файл:", file); // Проверим, выбран ли файл
-                      field.onChange(file); // Передаем файл в react-hook-form
-                      if (handleChange) handleChange(e); // Дополнительный обработчик
-                    }}
-                    disabled={disabled}
-                    className={error?.message && "border-[#BA1A1A]"}
-                  />
-                  {field.value && (
-                    <div className="text-sm mt-2 text-gray-500 absolute top-8">
-                      Выбранный файл: {field.value.name}
-                    </div>
+                  className={cn(
+                    error && "border-[#BA1A1A]",
+                    onPrimary &&
+                      "border-primary_outline_reverse focus:border-white hover:border-white focus:border-1 text-on_primary"
                   )}
-                </div>
+                />
+              ) : (
+                !textArea &&
+                type !== "file" && (
+                  <div className="relative">
+                    <Input
+                      ref={ref}
+                      type="file"
+                      placeholder={placeholder}
+                      onChange={(e) => {
+                        const file = e.target.files?.[0] || null;
+                        console.log("Выбранный файл:", file); // Проверим, выбран ли файл
+                        field.onChange(file); // Передаем файл в react-hook-form
+                        if (handleChange) handleChange(e); // Дополнительный обработчик
+                      }}
+                      disabled={disabled}
+                      className={error?.message && "border-[#BA1A1A]"}
+                    />
+                    {field.value && (
+                      <div className="text-sm mt-2 text-gray-500 absolute top-8">
+                        Выбранный файл: {field.value.name}
+                      </div>
+                    )}
+                  </div>
+                )
               )}
             </>
           </FormControl>
           <FormMessage
             className={cn(
-              "absolute -bottom-5 left-0 text-sm font-medium leading-[130%] ",
-              Boolean(error) && "text-[#BA1A1A]"
+              "absolute -bottom-5 left-0 text-sm font-medium leading-[130%]",
+              Boolean(error) && onPrimary
+                ? "text-teritary_04"
+                : "text-[#BA1A1A]"
             )}
           >
             {error ? error.message : supporText}
