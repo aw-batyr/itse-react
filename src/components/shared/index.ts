c067322cd8f45eec65bcfae07d3b84803e0d37d7
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-28 17:10:06 +0500

    reexport b2b-form

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -8,7 +8,7 @@ export { ContactCard } from "./contact-card";
 export { TimeCard } from "./time-card";
 export { OfferCard } from "./offer-card";
 export { EmblaDots } from "./embla-dots";
-export { B2bForm } from "./b2b-form";
+export { B2bForm } from "../forms/b2b-form";
 export { Field } from "./field";
 export { B2bFormProgress } from "./b2b-form-progress";
 export { FormSuccesStatus } from "./form-succes-status";
