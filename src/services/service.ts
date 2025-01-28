42f307fd29084dea918303e3c976ced6f24ff61b
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-28 23:21:50 +0500

    post service for the form

---
+++ src/services/service.ts
@@ -0,0 +1,23 @@
+import { SponsorFormType } from "@/lib/get-sponsor-form-details";
+import { StandFormType } from "@/lib/get-stend-form-details";
+import axios from "axios";
+
+const URL = "https://itse.turkmenexpo.com/app/api/v1";
+
+export const postStend = async (data: StandFormType): Promise<boolean> => {
+  const res = axios.post(`${URL}/become_delegate`, data);
+
+  return (await res).status === 201;
+};
+
+export const postB2b = async (data: FormData): Promise<boolean> => {
+  const res = axios.post(`${URL}/form`, data);
+
+  return (await res).status === 201;
+};
+
+export const postSponsor = async (data: SponsorFormType): Promise<boolean> => {
+  const res = axios.post(`${URL}/become_sponsor`, data);
+
+  return (await res).status === 201;
+};
