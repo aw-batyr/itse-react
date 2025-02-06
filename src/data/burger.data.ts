704caa5313394c5c562bad1bfaeba4a99ff3cd38
Batyr <awbatyr@gmail.com>
2025-02-06 17:30:02 +0500

    data for burger nav

---
+++ src/data/burger.data.ts
@@ -0,0 +1,53 @@
+export const burgerData = [
+  {
+    data: [
+      {
+        title: "О выставке",
+        link: "/about",
+      },
+      {
+        title: "Посетителям",
+        link: "",
+      },
+      {
+        title: "Экспонентам",
+        link: "",
+      },
+
+      {
+        title: "Медиа",
+        link: "",
+      },
+      {
+        title: "Контакты",
+        link: "/contacts",
+      },
+    ],
+  },
+
+  {
+    data: [
+      {
+        title: "About exhibition",
+        link: "/about",
+      },
+      {
+        title: "To the Visitors",
+        link: "",
+      },
+      {
+        title: "To the Exhibitors",
+        link: "",
+      },
+
+      {
+        title: "Media",
+        link: "",
+      },
+      {
+        title: "Contacts",
+        link: "/contacts",
+      },
+    ],
+  },
+];
