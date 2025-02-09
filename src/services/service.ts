31db8667d6ce748d528e9e99e51b8ae8da57f865
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-02-09 17:05:47 +0500

    added axios config

--- src/services/service.ts
+++ src/services/service.ts
@@ -1,9 +1,22 @@
+import { TimeType } from "@/hooks/tanstack/use-exhibition-time";
 import { ContactsFormType } from "@/lib/get-contacts-details";
 import { SponsorFormType } from "@/lib/get-sponsor-form-details";
 import { StandFormType } from "@/lib/get-stend-form-details";
+import { LangState, Language, useLangStore } from "@/store/lang";
 import axios from "axios";
 
 const URL = "https://itse.turkmenexpo.com/app/api/v1";
+
+// const API_CONFIG = {
+//   BASE_URL: "https://itse.turkmenexpo.com/app/api/v1",
+//   SUCCESS_STATUS: [200, 201],
+// };
+
+// const apiClient = axios.create({
+//   baseURL: API_CONFIG.BASE_URL,
+// });
+
+const lang = useLangStore.getState().lang;
 
 export const postStend = async (data: StandFormType): Promise<boolean> => {
   const res = axios.post(`${URL}/book_stand_form`, data);
@@ -29,8 +42,12 @@ export const postContact = async (data: ContactsFormType): Promise<boolean> => {
   return (await res).status === 201;
 };
 
-export const getExhibitionTime = async () => {
-  const data = axios.get<any>(`${URL}/exhibition_time`);
+export const getExhibitionTime = async (lang: LangState["lang"]) => {
+  const data = axios.get<TimeType>(`${URL}/exhibition_time`, {
+    headers: {
+      "Accept-Language": lang,
+    },
+  });
 
-  return data as any;
+  return data;
 };
