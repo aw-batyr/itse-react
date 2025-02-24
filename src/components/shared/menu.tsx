cecbcd65d94fa789d2b38a86477b22ceaec5abda
Batyr <awbatyr@gmail.com>
2025-02-24 16:20:17 +0500

    added triggerClassName and added type for dropDownContent

--- src/components/shared/menu.tsx
+++ src/components/shared/menu.tsx
@@ -2,30 +2,39 @@ import { FC, useState } from "react";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Chevron, Modal } from "./";
 import { Link } from "react-router-dom";
+import { DropDownContent } from "@/locales/types/nav.type";
+import { cn } from "@/lib/utils";
 
 interface Props {
+  triggerClassName?: string;
   className?: string;
   title: string;
   color?: string;
 
   onMenu?: VoidFunction;
 
-  dropDownContent?: {
-    modal?: boolean;
-    text: string;
-    link?: string;
-    blank?: boolean;
-  }[];
+  dropDownContent?: DropDownContent[];
 }
 
-export const Menu: FC<Props> = ({ title, dropDownContent, color, onMenu }) => {
+export const Menu: FC<Props> = ({
+  title,
+  dropDownContent,
+  color,
+  onMenu,
+  triggerClassName,
+}) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <Popover open={isOpen} onOpenChange={() => setIsOpen(!isOpen)}>
-      <PopoverTrigger className="flex items-center gap-2">
+      <PopoverTrigger
+        className={cn("flex items-center gap-2", triggerClassName)}
+      >
         {title}
-        <Chevron color={color} />
+        <Chevron
+          color={color}
+          className={cn("transition-all duration-200", isOpen && "rotate-180")}
+        />
       </PopoverTrigger>
 
       <PopoverContent className="w-fit px-0 py-2 cursor-pointer bg-slate-100">
