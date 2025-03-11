bc55c7c57861035b429432b4aaa7ba375d531a8c
Batyr <awbatyr@gmail.com>
2025-03-11 12:42:34 +0500

    added news and news inner reexport

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,7 @@
 export { Home } from "./home";
 export { B2b } from "./b2b";
+export { News } from "./news";
 export { StendForm } from "./stend-form";
 export { About } from "./about";
 export { Contacts } from "./contacts";
+export { NewsInner } from "./news-inner";
